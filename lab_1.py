9b448dff0e997c9236482f877ea5b9c929159dde
j-chrn <yulka-chernyavskaya005@yandex.ru>
2026-03-03 19:04:28 +0700

    Добавлено поле 'Цвет'

--- lab_1.py
+++ lab_1.py
@@ -3,17 +3,19 @@ import shlex
 
 
 class Income:
-    def __init__(self, date, source, amount):
+    def __init__(self, date, source, amount, color):
         self.date = date
         self.source = source
         self.amount = amount
+        self.color = color
 
     def __str__(self):
         return (
             "Объект типа 'Доход'\n"
             f"Дата: {self.date.strftime('%Y.%m.%d')}\n"
             f"Источник: {self.source}\n"
-            f"Сумма: {self.amount}"
+            f"Сумма: {self.amount}\n"
+            f"Цвет: {self.color}\n"
         )
 
 
@@ -25,20 +27,20 @@ def parse_income(input_string):
     data = input_string[len(prefix):].strip()
     parts = shlex.split(data)
 
-    if len(parts) != 3:
+    if len(parts) != 4:
         raise ValueError
 
-    date_str, source, amount_str = parts
+    date_str, source, amount_str, color = parts
 
     date = datetime.strptime(date_str, "%Y.%m.%d").date()
     amount = int(amount_str)
 
-    return Income(date, source, amount)
+    return Income(date, source, amount, color)
 
 
 if __name__ == "__main__":
     user_input = input(
-        "Введите данные в формате: Доход: ГГГГ.ММ.ДД \"Источник\" Сумма\n"
+        "Введите данные в формате: Доход: ГГГГ.ММ.ДД \"Источник\" Сумма \"Цвет\"\n"
     )
     income = parse_income(user_input)
     print()
